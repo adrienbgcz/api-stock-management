3718a23974d76d65bce178fc9900c0f453245ae0
Enzo Averlant <enaverlant@gmail.com>
2022-10-31 11:28:23 +0100

    :wrench: config: Update config

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-import express from 'express'
-const app = express()
-const port = process.env.PORT || 3000
-import cors from  'cors'
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+import cors from "cors";
 
-import customerEndpoint from './controller/customer-controller.js'
-import deviceEndpoint from './controller/device-controller.js'
-import billEndpoint from './controller/bill-controller.js'
-import transactionEndpoint from './controller/transaction-controller.js'
+import customerEndpoint from "./controller/customer-controller.js";
+import deviceEndpoint from "./controller/device-controller.js";
+import billEndpoint from "./controller/bill-controller.js";
+import transactionEndpoint from "./controller/transaction-controller.js";
 
 var corsOptions = {
-    origin: "https://flutter-stock-management.herokuapp.com:3000"
+  origin: "https://flutter-stock-management.herokuapp.com",
 };
 
 app.use(cors(corsOptions));
 
-app.use(express.json())
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-
-app.use('/', customerEndpoint)
-app.use('/', deviceEndpoint)
-app.use('/', billEndpoint)
-app.use('/', transactionEndpoint)
-
+app.use("/", customerEndpoint);
+app.use("/", deviceEndpoint);
+app.use("/", billEndpoint);
+app.use("/", transactionEndpoint);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}!`)
+  console.log(`App listening on port ${port}!`);
 });
-
-
