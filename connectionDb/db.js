d9a171516838612acc728bd6b1c3369229ac6c8b
Adrien Bogacz <ad.bogacz@gmail.com>
2023-01-13 15:32:43 +0100

    :sparkles: add environnement variables

--- connectionDb/db.js
+++ connectionDb/db.js
@@ -2,11 +2,11 @@ import pkg from 'pg';
 const {Pool} = pkg
 
 const db = new Pool({
-    user : "enzoavlt_stock_management",
-    host : "postgresql-enzoavlt.alwaysdata.net",
-    database : "enzoavlt_stock_management",
-    password : "flutterproject",
-    port : 5432
+    user : process.env.DB_USER,
+    host : process.env.DB_HOST,
+    database : process.env.DB_NAME,
+    password : process.env.DB_PASSWORD,
+    port : process.env.PORT
 })
 
 export default db;
