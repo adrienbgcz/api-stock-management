4855518c466d35a43f767c735480c6a08581916d
Adrien Bogacz <ad.bogacz@gmail.com>
2023-02-08 09:33:23 +0100

    :bug: correct env variables

--- connectionDb/db.js
+++ connectionDb/db.js
@@ -6,8 +6,16 @@ const db = new Pool({
     host : process.env.DB_HOST,
     database : process.env.DB_NAME,
     password : process.env.DB_PASSWORD,
-    port : process.env.PORT
+    port : process.env.DB_PORT
 })
+
+/*const db = new Pool({
+    user : "enzoavlt_stock_management",
+    host : "postgresql-enzoavlt.alwaysdata.net",
+    database : "enzoavlt_stock_management",
+    password : "flutterproject",
+    port : 5432
+})*/
 
 export default db;
 
