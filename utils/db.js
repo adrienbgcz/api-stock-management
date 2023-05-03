efc9b0aa5aa9171763e070004b39f81ca2c89b68
Adrien Bogacz <ad.bogacz@gmail.com>
2023-05-03 11:46:32 +0200

    :sparkles: get secret manager values

--- utils/db.js
+++ utils/db.js
@@ -2,21 +2,21 @@ import pkg from 'pg'
 import GcpSecrets from '../utils/gcp-secrets.js'
 const {Pool} = pkg
 
-const db = new Pool({
+/*const db = new Pool({
     user : GcpSecrets.getSecretValue(process.env.DB_USER),
     host : GcpSecrets.getSecretValue(process.env.DB_HOST),
     database : GcpSecrets.getSecretValue(process.env.DB_NAME),
     password : GcpSecrets.getSecretValue(process.env.DB_PASSWORD),
     port : GcpSecrets.getSecretValue(process.env.DB_PORT)
-})
+})*/
 
-/*const db = new Pool({
+const db = new Pool({
     user : await GcpSecrets.getSecretValue("db_user"),
     host : await GcpSecrets.getSecretValue("db_host"),
     database : await GcpSecrets.getSecretValue("db_name"),
     password : await GcpSecrets.getSecretValue("db_password"),
     port : await GcpSecrets.getSecretValue("db_port")
-})*/
+})
 
 /*const db = new Pool({
     user : "enzoavlt_stock_management",
