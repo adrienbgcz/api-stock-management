104b74c0df06b164b8a4312679fa0d742fd7603b
Adrien Bogacz <ad.bogacz@gmail.com>
2023-05-03 12:11:47 +0200

    :sparkles: secret access token on secret manager

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken'
 import GcpSecrets from '../utils/gcp-secrets.js'
 
 export default {
-    authenticateToken(req, res, next) {
+    async authenticateToken(req, res, next) {
         if(req.headers && req.headers.authorization) {
             try {
                 const token = req.headers.authorization.split(' ')[1]
-                const decodedToken = jwt.verify(token, GcpSecrets.getSecretValue(process.env.SECRET_ACCESS_TOKEN))
+                const decodedToken = jwt.verify(token, await GcpSecrets.getSecretValue(process.env.SECRET_ACCESS_TOKEN))
                 const userId = decodedToken.userId
                 req.auth = {
                     userId
