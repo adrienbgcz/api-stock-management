5a7708f37b0224aedbf2814d1500061617faeecd
Adrien Bogacz <ad.bogacz@gmail.com>
2023-05-05 14:30:13 +0200

    :construction: test token

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ export default {
             try {
                 const token = req.headers.authorization.split(' ')[1]
                 console.log("TOKEN", token)
-                const decodedToken = jwt.verify(token /*await GcpSecrets.getSecretValue(process.env.SECRET_ACCESS_TOKEN*/)
+                const decodedToken = jwt.verify(token, await GcpSecrets.getSecretValue(process.env.SECRET_ACCESS_TOKEN))
                 console.log("DECODED TOKEN", decodedToken)
                 /*const userId = decodedToken.userId
                 req.auth = {
