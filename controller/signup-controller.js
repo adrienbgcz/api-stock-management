a72f8c769c95d163dd823bceb353fa7cbf5a65c7
Adrien Bogacz <ad.bogacz@gmail.com>
2023-05-05 10:04:43 +0200

    :sparkles: add signup path

--- controller/signup-controller.js
+++ controller/signup-controller.js
@@ -2,14 +2,23 @@ import express from "express";
 const router = express.Router();
 import signupServices from '../service/signup-service.js'
 import loginService from "../service/login-service.js";
+import jwt from "jsonwebtoken";
+import GcpSecrets from "../utils/gcp-secrets.js";
 
 
 router.post('/signup', async (req, res) => {
-    let user = req.body
+    let userToCreate = req.body
     try {
-        await signupServices.createUser(user);
-        await loginService.login(user.email, user.password)
-        /*res.status(200).send()*/
+        await signupServices.createUser(userToCreate);
+        const user = await loginService.login(userToCreate.email, userToCreate.password)
+        res.status(200).json({
+            user,
+            token: jwt.sign(
+                { userId : user.id },
+                await GcpSecrets.getSecretValue(process.env.SECRET_ACCESS_TOKEN),
+                { expiresIn: '12h' }
+            )
+        })
     } catch (e) {
         console.error(e)
         res.status(500).send('An error occurred during user sign up')
