aa68f2934b397866f0198aba3a48da58234eaa5d
Adrien Bogacz <ad.bogacz@gmail.com>
2023-05-22 07:35:30 +0200

    :sparkles: add express validator on all routers, create unit tests and valid user id sent with token payload

--- middleware/express-validator/transactionRules.js
+++ middleware/express-validator/transactionRules.js
@@ -3,7 +3,6 @@ const { body } = validator
 
 export default {
     validationRules() {
-        console.log(body())
         return [
             body().isArray().withMessage('Invalid array format')
         ]
--- middleware/express-validator/validator.js
+++ middleware/express-validator/validator.js
@@ -3,11 +3,10 @@ import {validationResult} from "express-validator";
 export default {
   validate (req, res, next) {
     const errors = validationResult(req);
-    if (!req.body.length) {
+    if (!req.body.length || !Object.keys(req.body)) {
       return res.status(400).json({ error: 'Request body is empty' });
     }
     if (!errors.isEmpty()) return res.status(400).json(errors)
     else next();
   }
 }
-
